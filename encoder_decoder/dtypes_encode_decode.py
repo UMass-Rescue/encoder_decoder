87fbba433c55267f91cc74a95afd4494c32bb261
prasannals <prasanna.pessu@gmail.com>
2020-03-05 19:58:40 -0500

    Changed encoding of float ndarray to python list

--- encoder_decoder/dtypes_encode_decode.py
+++ encoder_decoder/dtypes_encode_decode.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pdb
 def float_ndarray_to_dict(arr):
     return np_arr_to_dict(arr)
 
@@ -16,11 +16,12 @@ def string_to_float(string):
     return float(string)
 
 def np_arr_to_dict(arr):
-    return {'str': np.ndarray.tostring(arr),
+    # pdb.set_trace()
+    return {'arr': arr.tolist(),
         'shape':list(arr.shape),
         'dtype':str(arr.dtype)
     }
 
 def dict_to_np_arr(data):
-    arr, shape, dtype = data['str'], data['shape'], data['dtype']
-    return np.fromstring(arr, dtype=dtype).reshape(tuple(shape))
+    arr, shape, dtype = data['arr'], data['shape'], data['dtype']
+    return np.array(arr, dtype=dtype).reshape(tuple(shape))
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='encoder_decoder',
-      version='0.1.2',
+      version='0.1.4',
       description='Functions to encode and decode commonly used data formats to support transmission of data over the web.',
       url='https://github.com/UMass-Rescue/encoder_decoder',
       author='Jagath Jai Kumar, Prasanna Lakkur Subramanyam',
